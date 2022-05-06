f4a9e01658ea6b3ab684851d05716827118ffd2d
TemchikCO <92385184+TemchikCO@users.noreply.github.com>
2022-05-06 13:04:49 +0300

    Delete CommentForm.py

--- CommentForm.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-
-class CommentForm(FlaskForm):
-    comments = TextAreaField("Ваши комментарии:", validators=[DataRequired()])
-    submit = SubmitField('Сохранить')
